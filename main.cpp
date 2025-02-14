50634f47576bcc644a5b0f087ecbed8ecee602a7
Irina <ira.kalachina@yandex.ru>
2025-02-14 21:07:30 +0700

    Вычисление лучшего индивида

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <Windows.h>
 #include <cstdlib>
@@ -5,40 +6,56 @@
 using namespace std;
 
 void create_arr(int len, int N) {
-    srand(time(0)); // Инициализация генератора случайных чисел
-    int** arr = new int*[N];
-    int* sum = new int[N](); // Массив для хранения суммы строк
+    srand(time(0));
+    int** population = new int*[N];
+    int* fitness = new int[N](); // Массив для хранения суммы строк (функция пригодности)
+    int best_index = 0; // Индекс лучшего индивида
 
+    // Генерация популяции и подсчет функции пригодности
     for (int i = 0; i < N; i++) {
-        arr[i] = new int[len];
+        population[i] = new int[len];
         for (int j = 0; j < len; j++) {
-            arr[i][j] = rand() % 2; // Генерация случайных 0 и 1
-            sum[i] += arr[i][j]; // Подсчет суммы элементов в строке
+            population[i][j] = rand() % 2; // Генерация случайных 0 и 1
+            fitness[i] += population[i][j]; // Подсчет суммы элементов в строке
+        }
+        // Определение индекса индивида с наибольшей функцией пригодности
+        if (fitness[i] > fitness[best_index]) {
+            best_index = i;
         }
     }
+
+    // Находим максимальное значение функции пригодности
+    int best_fitness = fitness[best_index];
 
     // Вывод массива с суммами строк
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < len; j++) {
-            cout << arr[i][j] << " ";
+            cout << population[i][j] << " ";
         }
-        cout << "\t" << sum[i];
+        cout << "\t" << fitness[i];
         cout << endl;
     }
 
+    // Вывод лучшего индивида
+    cout << "Лучший индивид: ";
+    for (int j = 0; j < len; j++) {
+        cout << population[best_index][j] << " "; // Вывод полной строки лучшего индивида
+    }
+    cout << "\t" << best_fitness << endl; // Вывод максимальной функции пригодности
+
     // Освобождение памяти
     for (int i = 0; i < N; i++) {
-        delete[] arr[i];
+        delete[] population[i];
     }
-    delete[] arr;
-    delete[] sum;
+    delete[] population;
+    delete[] fitness;
 }
 
 int main() {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
-    int N = 10;
-    int len = 8;
+    int N = 10; // Количество строк
+    int len = 8; // Длина каждой строки
     create_arr(len, N);
     return 0;
 }
