7fbdabba592167a59318c75dd8e2439cae18aa04
Irina <ira.kalachina@yandex.ru>
2025-02-15 14:53:14 +0700

    Селекция

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <ctime>
 using namespace std;
 
-void create_arr(int len, int N) {
+void create_arr(int len, int N, int tournir) {
     srand(time(0));
     int** population = new int*[N];
     int* fitness = new int[N](); // Массив для хранения суммы строк (функция пригодности)
@@ -23,7 +23,6 @@ void create_arr(int len, int N) {
             best_index = i;
         }
     }
-
     // Находим максимальное значение функции пригодности
     int best_fitness = fitness[best_index];
 
@@ -39,16 +38,54 @@ void create_arr(int len, int N) {
     // Вывод лучшего индивида
     cout << "Лучший индивид: ";
     for (int j = 0; j < len; j++) {
-        cout << population[best_index][j] << " "; // Вывод полной строки лучшего индивида
+        cout << population[best_index][j] << " ";
     }
-    cout << "\t" << best_fitness << endl; // Вывод максимальной функции пригодности
+    cout << "\t" << best_fitness << endl;
+
+    int *tournir_list = new int[len]();
+    int k, p = len+10;
+    for (int i = 0; i < N; i++) {
+        do{
+            k = rand() % (len-1);
+            p = rand() % (len-1);
+        } while (p == k);
+        if (fitness[k] >= fitness[p]){
+            tournir_list[i]=k;
+        }
+        else{tournir_list[i]=p;}
+        /*турнирная селекция*/
+    }
+    //вывод массива победителей турнирной селекции
+    /*for (int i = 0; i < N; i++) {
+        cout << tournir_list[i] << " ";
+    }
+    cout << endl;*/
+
+    // Селекция
+    int** selection = new int*[N];
+    for (int i = 0; i < N; i++) {
+        selection[i] = new int[len];
+        int a = tournir_list[i];
+        for (int j = 0; j < len; j++) {
+            selection[i][j] = population[a][j];
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < len; j++) {
+            cout << selection[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     // Освобождение памяти
     for (int i = 0; i < N; i++) {
         delete[] population[i];
+        delete[] selection[i];
     }
     delete[] population;
     delete[] fitness;
+    delete[] tournir_list;
+    delete[] selection;
 }
 
 int main() {
@@ -56,6 +93,7 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     int N = 10; // Количество строк
     int len = 8; // Длина каждой строки
-    create_arr(len, N);
+    int tournir = 2; // Количество индивидов, принимающих участие в турнире
+    create_arr(len, N, tournir);
     return 0;
 }
